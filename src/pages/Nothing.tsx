1aee7fa13758d057fa9559cfd14ccd3a35278f17
ganbowen <ganbowen@baiwang.com>
2022-07-14 10:02:11 +0800

    feat: 添加css动画

--- src/pages/Nothing.tsx
+++ src/pages/Nothing.tsx
@@ -2,14 +2,19 @@
  * @Author       : ganbowen
  * @Date         : 2022-07-13 13:50:33
  * @LastEditors  : ganbowen
- * @LastEditTime : 2022-07-13 13:51:17
+ * @LastEditTime : 2022-07-14 10:00:53
  * @Descripttion : 404
  */
-
+import {
+   Link
+  } from "react-router-dom";
 export default function Nothing() {
     return (
       <div>
-        <h2>Nothing</h2>
+        <h2 className="nothing">Nothing</h2>
+        <div className="nothing-to-home">
+            <Link to='/game'>去大厅...</Link>
+        </div>
       </div>
     );
 }
